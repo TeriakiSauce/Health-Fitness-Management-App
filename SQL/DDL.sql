2c640e67b9e4e5baa4c04fa20fc46af06f4a831e
Tarik B <48898220+TeriakiSauce@users.noreply.github.com>
2024-04-12 14:38:43 -0400

    Changed order of statements
    Tables with no foreign keys are created first

--- SQL/DDL.sql
+++ SQL/DDL.sql
@@ -9,6 +9,18 @@ CREATE TABLE Member (
     fitness_goals VARCHAR(255) NOT NULL
 );
 
+CREATE TABLE Trainer (
+    id SERIAL PRIMARY KEY,
+    schedule VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE AdministrativeStaff (
+    id SERIAL PRIMARY KEY,
+    rooms VARCHAR(255) NOT NULL,
+    fitness_equipment VARCHAR(255) NOT NULL,
+    class_schedules VARCHAR(255) NOT NULL
+);
+
 CREATE TABLE SchedulesWith (
     member_id SERIAL PRIMARY KEY,
     trainer_id INT,
@@ -21,11 +33,6 @@ CREATE TABLE SearchesFor (
    FOREIGN KEY (trainer_id) REFERENCES Trainer(id)
 );
 
-CREATE TABLE Trainer (
-    id SERIAL PRIMARY KEY,
-    schedule VARCHAR(255) NOT NULL
-);
-
 CREATE TABLE ManagesBilling (
     transaction_id SERIAL PRIMARY KEY,
     admin_id INT,
@@ -33,11 +40,4 @@ CREATE TABLE ManagesBilling (
     card_type VARCHAR(255) NOT NULL,
     card_num VARCHAR(255) NOT NULL,
     FOREIGN KEY (admin_id) REFERENCES AdministrativeStaff(id)
-);
-
-CREATE TABLE AdministrativeStaff (
-    id SERIAL PRIMARY KEY,
-    rooms VARCHAR(255) NOT NULL,
-    fitness_equipment VARCHAR(255) NOT NULL,
-    class_schedules VARCHAR(255) NOT NULL
 );
