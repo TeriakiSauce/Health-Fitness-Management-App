e85baf5a5f22dcc20cbc97ed16c0b00733033d86
muhammadhassan1102 <91349783+muhammadhassan1102@users.noreply.github.com>
2024-04-13 13:07:19 -0400

    Update DDL.sql

--- SQL/DDL.sql
+++ SQL/DDL.sql
@@ -6,7 +6,7 @@ CREATE TABLE Member (
     Height NUMERIC NOT NULL,
     Weight NUMERIC NOT NULL,
     WeightGoal NUMERIC NOT NULL,
-    WeightChange INT NOT NULL
+    WeightChange INTEGER NOT NULL
 );
 
 CREATE TABLE Trainer (
