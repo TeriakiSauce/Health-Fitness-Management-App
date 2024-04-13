01fe97492724190af09bafca6c0e698e71a3a7f0
Caleb Lui-Yee <113129278+Waluiyeegi@users.noreply.github.com>
2024-04-12 22:25:03 -0400

    fixed ddl

--- SQL/DDL.sql
+++ SQL/DDL.sql
@@ -1,43 +1,64 @@
--- COMP 3005 Project V2 Group 95 DDL
-
 CREATE TABLE Member (
-    member_id SERIAL PRIMARY KEY,
-    schedule VARCHAR(255) NOT NULL,
-    dashboard VARCHAR(255) NOT NULL,
-    personal_info VARCHAR(255) NOT NULL,
-    health_metrics VARCHAR(255) NOT NULL,
-    fitness_goals VARCHAR(255) NOT NULL
+    ID SERIAL PRIMARY KEY,
+    FullName VARCHAR(255) NOT NULL,
+    Email VARCHAR(255) NOT NULL,
+    Phone VARCHAR(20) NOT NULL,
+    Height NUMERIC NOT NULL,
+    Weight NUMERIC NOT NULL,
+    WeightGoal NUMERIC NOT NULL
 );
 
 CREATE TABLE Trainer (
-    id SERIAL PRIMARY KEY,
-    schedule VARCHAR(255) NOT NULL
+    ID SERIAL PRIMARY KEY,
+    FullName VARCHAR(255) NOT NULL,
+    DaysAvailable VARCHAR(255) NOT NULL,
+    StartTime VARCHAR(255) NOT NULL,
+    EndTime VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE AdministrativeStaff (
-    id SERIAL PRIMARY KEY,
-    rooms VARCHAR(255) NOT NULL,
-    fitness_equipment VARCHAR(255) NOT NULL,
-    class_schedules VARCHAR(255) NOT NULL
+    ID SERIAL PRIMARY KEY,
+    FullName VARCHAR(255) NOT NULL,
+    Email VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE SchedulesWith (
-    member_id SERIAL PRIMARY KEY,
-    trainer_id INT,
-    FOREIGN KEY (trainer_id) REFERENCES Trainer(id)
+CREATE TABLE TrainingSession (
+    ID SERIAL PRIMARY KEY,
+    MemberID INTEGER REFERENCES Member(ID),
+    TrainerID INTEGER REFERENCES Trainer(ID),
+    SessionDay VARCHAR(255) NOT NULL,
+    StartTime VARCHAR(255) NOT NULL,
+    EndTime VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE SearchesFor (
-   member_id SERIAL PRIMARY KEY,
-   trainer_id INT,
-   FOREIGN KEY (trainer_id) REFERENCES Trainer(id)
+CREATE TABLE RoomBooking (
+    ID SERIAL PRIMARY KEY,
+    RoomID INTEGER,
+    MemberID INTEGER REFERENCES Member(ID),
+    BookingDay VARCHAR(255) NOT NULL,
+    StartTime VARCHAR(255) NOT NULL,
+    EndTime VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE ManagesBilling (
-    transaction_id SERIAL PRIMARY KEY,
-    admin_id INT,
-    amount FLOAT,
-    card_type VARCHAR(255) NOT NULL,
-    card_num VARCHAR(255) NOT NULL,
-    FOREIGN KEY (admin_id) REFERENCES AdministrativeStaff(id)
+CREATE TABLE Equipment (
+    ID SERIAL PRIMARY KEY,
+    EquipmentName VARCHAR(255) NOT NULL,
+    MaintenanceStatus VARCHAR(255) NOT NULL,
+    Availability VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE Class (
+    ID SERIAL PRIMARY KEY,
+    ClassName VARCHAR(255) NOT NULL,
+    ClassDay VARCHAR(255) NOT NULL,
+    StartTime VARCHAR(255) NOT NULL,
+    EndTime VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE Billing (
+    ID SERIAL PRIMARY KEY,
+    MemberID INTEGER REFERENCES Member(ID),
+    Amount NUMERIC NOT NULL,
+    CardNumber VARCHAR(255) NOT NULL,
+    CardType VARCHAR(255) NOT NULL
 );
